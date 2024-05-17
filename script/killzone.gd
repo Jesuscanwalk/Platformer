7b8334f497742fb5daddedc88ee6dae6e439c06a
Jesuscanwalk <beliu@s.sfusd.edu>
2024-05-17 15:03:08 -0700

    Dying sound

--- script/killzone.gd
+++ script/killzone.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
+	$AudioStreamPlayer.play()
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
